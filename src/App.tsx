f6206eae1cbc0c413341603e5566583cc6689a82
Jon-Cactus <jonathantcspence@gmail.com>
2025-06-07 15:43:57 +0900

    Added menu screen to select local or online play (before branching)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import Game from './components/Game';
+import {useState} from 'react';
+import LocalGame from './components/LocalGame';
 import './App.css';
 
+type Mode = 'local' | 'online' | null;
+
+function Header() {
+  return (
+    <header className="nav-bar">
+      <h1>Ultimate Tic-Tac-Toe</h1>
+    </header>
+  )
+  //<button className="nav-item"></button>
+  //<button className="nav-item"></button>
+}
+
 export default function App() {
+  // Control local/online play
+  const [mode, setMode] = useState<Mode>(null);
+
+  if (mode === null) {
+    return (
+      <>
+        <Header />
+        <div className="game-selector">
+          <button className="btn-base" onClick={() => setMode('local')}>Play Locally</button>
+          <button className="btn-base" onClick={() => setMode('online')}>Play Online</button>
+        </div>
+      </>
+    )
+  }
   return (
     <div className="page">
-      <div className="hidden"></div>
-      <Game />
+      <Header />
+      {mode === 'local' ? (<LocalGame />) : (<OnlineLobby />)}
+      <LocalGame />
     </div>
-    
   )
 }
